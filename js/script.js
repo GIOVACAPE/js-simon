05e9924b0457099a5a3579f9815daac8dfba1878
Giovanni Capelli <giovacape01@gmail.com>
2023-01-16 10:43:32 +0100

    html+js step

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* STEP:
+1)definire una funzione per generare un numero casuale
+2)Generare 5 numeri casuali utilizzando la funzione appena definita
+3)inserire in pagina i 5 numeri appena generati
+4)definire yna funzione che gestisca il "GIOCO"
+5)far partire un timer di 30 secondi e specificare cosa defe fare dopo
+6) Al termine degli input  dell'utente, mostrare con un alert o in pagin il risultato-
+*/
