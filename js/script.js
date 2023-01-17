b905c8a187f00d2134d5b6183524bef294192944
Giovanni Capelli <giovacape01@gmail.com>
2023-01-17 16:55:51 +0100

    Js aggiustato. completato

--- js/script.js
+++ js/script.js
@@ -7,3 +7,42 @@
 6) Al termine degli input  dell'utente, mostrare con un alert o in pagin il risultato-
 */
 
+var numeriDaIndovinare = [];
+
+while (numeriDaIndovinare.length < 5) {
+  var numeroRandom = Math.floor(Math.random() * (100 - 1 + 1) ) + 1;
+  if (!numeriDaIndovinare.includes(numeroRandom)) {
+    numeriDaIndovinare.push(numeroRandom);
+  }
+}
+
+console.log(numeriDaIndovinare);
+
+
+alert('Miraccomando ricordati questi numeri! \nHai 30 secondi:' + '\n' + numeriDaIndovinare);
+
+
+setTimeout(richiesta, 5000 );
+
+
+var numeriUtente =[];
+
+function richiesta() {
+  for (var i = 0; i < 5; i++) {
+    var chiediNumero = parseInt(prompt('Inserisci un numero!'));
+    if (!numeriUtente.includes(chiediNumero) && numeriDaIndovinare.includes(chiediNumero)) {
+      numeriUtente.push(chiediNumero);
+    }
+  }
+  console.log(numeriUtente);
+
+  if (numeriUtente.length == 0) {
+    console.log('sei scarsissimo, non ne hai azzeccato mezzo')
+  } else if (numeriUtente.length == 1) {
+    console.log(' Hai indovinato un solo numero ed è: ' + numeriUtente)
+  } else if (numeriUtente.length == 5) {
+    console.log('Grandissimo, li hai indovinati tutti! Come hai detto tu erano: ' + numeriUtente)
+  } else {
+    console.log('Hai indovinato ' + numeriUtente.length + ' numeri e sono: ' + numeriUtente);
+  }
+};
