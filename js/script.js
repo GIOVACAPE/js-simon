4c2209bff040f6df6b688fbad868ae0dabc27514
Giovanni Capelli <giovacape01@gmail.com>
2023-01-17 16:15:32 +0100

    manca set Timeout

--- js/script.js
+++ js/script.js
@@ -6,3 +6,22 @@
 5)far partire un timer di 30 secondi e specificare cosa defe fare dopo
 6) Al termine degli input  dell'utente, mostrare con un alert o in pagin il risultato-
 */
+
+function getRandomNumber(min, max) {
+    return Math.floor(math.random() * (max - min + 1)) + min;   
+}
+
+const numbers = [];
+
+for (let i = 0; i < 5;  i++) {
+    const randomNumber = getRandomNumber(1, 99);
+    console.log(randomNumber);
+
+    numbers.push(randomNumber);
+    
+}
+
+console.log(numbers);
+
+const numbersContainer = document.getElementById('numbers-container');
+numbersContainer.innerHTML = numbers;
